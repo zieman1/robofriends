cb8e15dbd0c2eae1f5bd9fe9dd63ee57309e177c
zieman1 <ziemanzahrin@gmail.com>
2018-12-11 23:20:59 +0800

    Adding starting files

---
+++ build/precache-manifest.1ec341fd5f3b1b58504134386bff5b2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "09a5f5c7f13175ae58d2",
+    "url": "/static/css/main.c0fc5d07.chunk.css"
+  },
+  {
+    "revision": "09a5f5c7f13175ae58d2",
+    "url": "/static/js/main.09a5f5c7.chunk.js"
+  },
+  {
+    "revision": "d8296136342825ac06ed",
+    "url": "/static/css/1.93c87b40.chunk.css"
+  },
+  {
+    "revision": "d8296136342825ac06ed",
+    "url": "/static/js/1.d8296136.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8d52b6cad30b0a1181beb3e4cdbfbf57",
+    "url": "/index.html"
+  }
+];
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Leanne',
+    email: 'leanne@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Ervin',
+    email: 'ervin@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Clementine',
+    email: 'clementine@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Patricia',
+    email: 'patricia.lebsack@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Chelsey',
+    email: 'chelsey@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Dennis Schulist',
+    username: 'Dennis',
+    email: 'dennis@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Kurtis',
+    email: 'kurtis@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Neuer',
+    username: 'Nicholas',
+    email: 'nicholas@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Glenna',
+    email: 'glenna@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Augustin Dupuis',
+    username: 'Augustin',
+    email: 'augustin@karina.biz'
+  }
+];
